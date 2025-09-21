156cd52b773797c68c11a6f0aaee79b3ee0860f0
itsvishu0070 <124810186+itsvishu0070@users.noreply.github.com>
2025-09-21 17:46:36 +0530

    Update tripRoutes.js

--- server/routes/tripRoutes.js
+++ server/routes/tripRoutes.js
@@ -27,31 +27,3 @@ export default router;
 
 
 
-// import express from "express";
-// import {
-//   createTrip,
-//   getTrips,
-//   getTripById,
-//   updateTrip,
-//   deleteTrip,
-//   getTripBookings,
-// } from "../controllers/tripController.js";
-// import { protect, admin } from "../middleware/authMiddleware.js";
-// import upload from "../config/cloudinaryConfig.js";
-
-// const router = express.Router();
-
-// // Public routes
-// router.get("/", getTrips);
-// router.get("/:id", getTripById);
-
-// // Admin-only routes
-// router.get("/:id/bookings", protect, admin, getTripBookings);
-// router.post("/", protect, admin, upload.single("imageUrl"), createTrip);
-
-// // ✅ FIXED: added upload.single("imageUrl") here
-// router.put("/:id", protect, admin, upload.single("imageUrl"), updateTrip);
-
-// router.delete("/:id", protect, admin, deleteTrip);
-
-// export default router;
